a4fb13b68f1bdb79e43dc6142b0c9c0e94e81ecc
Satish Maurya <satishmaurya30118@gmail.com>
2024-09-24 19:19:29 +0530

    fixes

--- Backend/server.js
+++ Backend/server.js
@@ -41,7 +41,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-sphere-theta.vercel.app",
     // credentials: true,
   },
 });

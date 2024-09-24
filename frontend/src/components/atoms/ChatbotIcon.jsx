12a158f1a7f263ca841f642651474b4110762515
satish30118 <118624360+satish30118@users.noreply.github.com>
2024-09-25 04:01:36 +0530

    Update ChatbotIcon.jsx

--- frontend/src/components/atoms/ChatbotIcon.jsx
+++ frontend/src/components/atoms/ChatbotIcon.jsx
@@ -3,18 +3,8 @@ import { FaCommentDots } from "react-icons/fa";
 
 const ChatbotIcon = () => {
   return (
-    <Box 
-      as="button" 
-      borderRadius="full" 
-      bg="teal.500" 
-      color="white" 
-      p={4} 
-      boxShadow="md"
-      _hover={{ bg: "teal.400" }}
-      w="20px" 
-      h="20px" 
-    >
-      <Icon as={FaCommentDots} w={8} h={8} />
+    <Box>
+      <Icon as={FaCommentDots} w={6} h={6} />
     </Box>
   );
 };
